2773233b148a9b88ee0dae1011b1be190b546021
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-09 13:38:09 +0300

    Complete lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+
+def parrot(sound = 'Squawk!'):
+    print(sound)
+    return sound
+
+# Calling the function with the default argument
+result = parrot()  
+print(result)       
+
+# Calling the function with a custom argument
+custom_result = parrot("Hello, world!")  
+print(custom_result)                     
